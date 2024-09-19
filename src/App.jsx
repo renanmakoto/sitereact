eab3aff5dc012beb6220c8824891b6c15f81ca58
Renan Makoto <65373130+renanmakoto@users.noreply.github.com>
2024-09-19 07:00:07 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import NavLogo from './components/NavLogo'
 import NavMenu from './components/NavMenu'
 import Home from './components/Home'
-import About from './components/About';
+import About from './components/About'
 import Skills from './components/Skills';
 import Services from './components/Services';
 import Contact from './components/Contact';
