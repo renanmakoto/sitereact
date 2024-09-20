0cac8bd8ef65794f6a95032b5b709d0de5b10417
Renan Makoto <65373130+renanmakoto@users.noreply.github.com>
2024-09-20 07:35:08 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Home from './components/Home'
 import About from './components/About'
 import Skills from './components/Skills'
 import Services from './components/Services'
-import Contact from './components/Contact';
+import Contact from './components/Contact'
 import Footer from './components/Footer';
 import ScrollUp from './components/ScrollUp';
 
