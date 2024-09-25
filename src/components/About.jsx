9d62fe13467d7626147e2f7bc82c3513a63b4410
Renan Makoto <65373130+renanmakoto@users.noreply.github.com>
2024-09-25 07:09:00 -0400

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import { LiaFileDownloadSolid } from "react-icons/lia"
 
-const About = () => {
-  const sectionStyle = {
+export default function About() {
+  
+  return (
+    <section style={sectionStyle}>
+      <h2 style={titleStyle}>About Me</h2>
+      <p style={descriptionStyle}>
+        As a dedicated developer with profound expertise in JavaScript, I excel in delivering meticulously crafted solutions that merge creativity with technical precision. My approach is anchored in methodical analysis and a deep attention to detail, ensuring that every project I undertake is executed with the highest level of accuracy and effectiveness. With a robust background in Linux systems, I leverage this platform to create secure, efficient, and scalable software environments. My extensive experience spans across designing intuitive front-end interfaces, developing resilient back-end systems, and optimizing performance across the stack. I am committed to continuous learning, constantly evolving my skill set to stay at the forefront of technology. I thrive in dynamic environments where I can apply my expertise to innovative projects, consistently pushing the boundaries of what's possible through the power of JavaScript and open-source tools.
+      </p>
+      <a href="/assets/renanmakotoresume.pdf" download="renanmakotoresume.pdf" style={buttonStyle}>
+        Download Resume <LiaFileDownloadSolid style={iconStyle} />
+      </a>
+    </section>
+  )
+}
+
+const sectionStyle = {
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
@@ -47,18 +61,3 @@ const About = () => {
   const iconStyle = {
     fontSize: '2.25rem',
   }
-
-  return (
-    <section style={sectionStyle}>
-      <h2 style={titleStyle}>About Me</h2>
-      <p style={descriptionStyle}>
-        As a dedicated developer with profound expertise in JavaScript, I excel in delivering meticulously crafted solutions that merge creativity with technical precision. My approach is anchored in methodical analysis and a deep attention to detail, ensuring that every project I undertake is executed with the highest level of accuracy and effectiveness. With a robust background in Linux systems, I leverage this platform to create secure, efficient, and scalable software environments. My extensive experience spans across designing intuitive front-end interfaces, developing resilient back-end systems, and optimizing performance across the stack. I am committed to continuous learning, constantly evolving my skill set to stay at the forefront of technology. I thrive in dynamic environments where I can apply my expertise to innovative projects, consistently pushing the boundaries of what's possible through the power of JavaScript and open-source tools.
-      </p>
-      <a href="/assets/renanmakotoresume.pdf" download="renanmakotoresume.pdf" style={buttonStyle}>
-        Download Resume <LiaFileDownloadSolid style={iconStyle} />
-      </a>
-    </section>
-  )
-}
-
-export default About
