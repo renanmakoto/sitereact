728314a50cd1c6f79ac57a74d781fa7d05049943
Renan Makoto <65373130+renanmakoto@users.noreply.github.com>
2024-09-30 13:03:32 -0400

    Update ScrollUp.jsx

--- src/components/ScrollUp.jsx
+++ src/components/ScrollUp.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import { FaArrowUp } from 'react-icons/fa'
 
-const ScrollUp = () => {
+
+export default function ScrollUp() {
   const [showScroll, setShowScroll] = useState(false)
 
   useEffect(() => {
